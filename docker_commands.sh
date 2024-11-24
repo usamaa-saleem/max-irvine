dd4d30971706c9e80d95b8a0e663a84da1e3eb78
Muhamamd Usama Saleem <127926848+usamaa-saleem@users.noreply.github.com>
2024-11-25 04:33:08 +0500

    Update docker_commands.sh

--- docker_commands.sh
+++ docker_commands.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Build the Docker image
-sudo docker build -t usamaasaleeem/max:latest .
+sudo docker build -t username/project:latest .
 if [ $? -ne 0 ]; then
     echo "Docker build failed."
     exit 1
 fi
 
-# Push the Docker image to the repository
-# sudo docker push usamaasaleeem/max:latest
-# if [ $? -ne 0 ]; then
-#     echo "Docker push failed."
-#     exit 1
-# fi
+Push the Docker image to the repository
+sudo docker push username/project:latest
+if [ $? -ne 0 ]; then
+    echo "Docker push failed."
+    exit 1
+fi
 
 echo "Docker build and push completed successfully."
